b2739ee6c07f0de9b95b2c6ea84fcb578d415940
Kate <ekaterina21051988@yandex.by>
2024-03-05 18:05:44 +0200

    feat: ArtistService and ArtistController have been added in the AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserController } from './users/controllers/user.controller';
 import { UserService } from './users/services/user.service';
+import { ArtistService } from './artists/services/artist.service';
+import { ArtistController } from './artists/controllers/artist.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, UserController],
-  providers: [AppService, UserService],
+  controllers: [AppController, UserController, ArtistController],
+  providers: [AppService, UserService, ArtistService],
 })
 export class AppModule {}
