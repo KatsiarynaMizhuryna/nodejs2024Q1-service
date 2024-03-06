4f70ad71716e866cbf29be54f0c2d64174b1e171
Kate <ekaterina21051988@yandex.by>
2024-03-06 19:24:36 +0200

    feat: logic for TrackService has been created

--- src/track/track.service.ts
+++ src/track/track.service.ts
@@ -1,26 +1,78 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateTrackDto } from './dto/create-track.dto';
 import { UpdateTrackDto } from './dto/update-track.dto';
+import { Track } from './entities/track.entity';
+import { v4 as uuidv4 } from 'uuid';
+import { isValidID } from '../helpers/id_validation';
 
 @Injectable()
 export class TrackService {
-  create(createTrackDto: CreateTrackDto) {
-    return 'This action adds a new track';
+  private tracks: Track[] = [];
+
+  create(createTrackDto: CreateTrackDto): Track {
+    const { name, artistId, albumId, duration } = createTrackDto;
+    if (!name || !duration) {
+      throw new BadRequestException('Name and Duration are required');
+    }
+
+    const newTrack: Track = {
+      id: uuidv4(),
+      name,
+      artistId,
+      albumId,
+      duration,
+    };
+    this.tracks.push(newTrack);
+    return newTrack;
   }
 
   findAll() {
-    return `This action returns all track`;
+    return this.tracks;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} track`;
+  findOne(id: string): Track {
+    isValidID(id);
+    const track = this.tracks.find((t) => t.id === id);
+    if (!track) {
+      throw new NotFoundException(`Track with ID ${id} not found`);
+    }
+    return track;
   }
 
-  update(id: number, updateTrackDto: UpdateTrackDto) {
-    return `This action updates a #${id} track`;
+  update(id: string, updateTrackDto: UpdateTrackDto): Track {
+    isValidID(id);
+    const track = this.findOne(id);
+    if (!track) {
+      throw new NotFoundException(`Track with ID ${id} not found`);
+    }
+    if (updateTrackDto.name) {
+      track.name = updateTrackDto.name;
+    }
+    if (updateTrackDto.artistId !== undefined) {
+      track.artistId = updateTrackDto.artistId;
+    }
+
+    if (updateTrackDto.albumId !== undefined) {
+      track.albumId = updateTrackDto.albumId;
+    }
+
+    if (updateTrackDto.duration !== undefined) {
+      track.duration = updateTrackDto.duration;
+    }
+
+    return track;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} track`;
+  remove(id: string) {
+    isValidID(id);
+    const trackIndex = this.tracks.findIndex((u) => u.id === id);
+    if (trackIndex === -1) {
+      throw new NotFoundException(`Track with ID ${id} not found`);
+    }
+    this.tracks.splice(trackIndex, 1);
   }
 }
