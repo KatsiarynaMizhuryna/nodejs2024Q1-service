4e46791edfd2c43546095363d27b99537c768f99
Kate <ekaterina21051988@yandex.by>
2024-03-06 19:28:23 +0200

    feat: AppModule has been updated

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserModule } from './users/user.module';
-import { ArtistModule } from './artists/artist.module';
-import { AlbumModule } from './albums/album.module';
-import { TrackModule } from './track/track.module';
 import { UserModule } from './user/user.module';
-import { AlbumModule } from './album/album.module';
 import { ArtistModule } from './artist/artist.module';
+import { AlbumModule } from './album/album.module';
+import { TrackModule } from './track/track.module';
 import { FavoriteModule } from './favorite/favorite.module';
 
 @Module({
