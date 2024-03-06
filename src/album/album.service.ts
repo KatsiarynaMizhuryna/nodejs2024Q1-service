5ddccd27ad1b2ca8af548c4f9dbcef0fba4c249d
Kate <ekaterina21051988@yandex.by>
2024-03-06 19:23:54 +0200

    feat: logic for AlbumService has been created

--- src/album/album.service.ts
+++ src/album/album.service.ts
@@ -1,26 +1,72 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateAlbumDto } from './dto/create-album.dto';
 import { UpdateAlbumDto } from './dto/update-album.dto';
+import { Album } from './entities/album.entity';
+import { v4 as uuidv4 } from 'uuid';
+import { isValidID } from '../helpers/id_validation';
 
 @Injectable()
 export class AlbumService {
-  create(createAlbumDto: CreateAlbumDto) {
-    return 'This action adds a new album';
+  private albums: Album[] = [];
+
+  create(createAlbumDto: CreateAlbumDto): Album {
+    const { name, year, artistId } = createAlbumDto;
+
+    if (!name || !year || !artistId) {
+      throw new BadRequestException('All fields are required');
+    }
+
+    const newAlbum: Album = {
+      id: uuidv4(),
+      name,
+      year,
+      artistId,
+    };
+    this.albums.push(newAlbum);
+    return newAlbum;
   }
 
   findAll() {
-    return `This action returns all album`;
+    return this.albums;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} album`;
+  findOne(id: string) {
+    isValidID(id);
+    const album = this.albums.find((a) => a.id === id);
+    if (!album) {
+      throw new NotFoundException(`Album with ID ${id} not found`);
+    }
+    return album;
   }
 
-  update(id: number, updateAlbumDto: UpdateAlbumDto) {
-    return `This action updates a #${id} album`;
+  update(id: string, updateAlbumDto: UpdateAlbumDto): Album {
+    isValidID(id);
+    const album = this.findOne(id);
+    if (!album) {
+      throw new NotFoundException(`Album with ID ${id} not found`);
+    }
+    if (updateAlbumDto.name) {
+      album.name = updateAlbumDto.name;
+    }
+    if (updateAlbumDto.year) {
+      album.year = updateAlbumDto.year;
+    }
+    if (updateAlbumDto.artistId !== undefined) {
+      album.artistId = updateAlbumDto.artistId;
+    }
+    return album;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} album`;
+  remove(id: string) {
+    isValidID(id);
+    const albumIndex = this.albums.findIndex((u) => u.id === id);
+    if (albumIndex === -1) {
+      throw new NotFoundException(`Album with ID ${id} not found`);
+    }
+    this.albums.splice(albumIndex, 1);
   }
 }
