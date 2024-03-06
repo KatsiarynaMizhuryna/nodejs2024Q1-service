2dc89a13d88f26072fcc909aa1184edb5bc0f69a
Kate <ekaterina21051988@yandex.by>
2024-03-06 17:45:56 +0200

    feat: ArtistService and ArtistController have been added in the AppModule

--- src/artist/artist.service.ts
+++ src/artist/artist.service.ts
@@ -1,26 +1,56 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateArtistDto } from './dto/create-artist.dto';
 import { UpdateArtistDto } from './dto/update-artist.dto';
+import { Artist } from './entities/artist.entity';
+import { v4 as uuidv4 } from 'uuid';
 
 @Injectable()
 export class ArtistService {
-  create(createArtistDto: CreateArtistDto) {
-    return 'This action adds a new artist';
+  private artists: Artist[] = [];
+
+  create(createArtistDto: CreateArtistDto): Artist {
+    const { name, grammy } = createArtistDto;
+    if (!name) {
+      throw new NotFoundException('Name is required');
+    }
+    const newArtist: Artist = {
+      id: uuidv4(),
+      name,
+      grammy,
+    };
+
+    this.artists.push(newArtist);
+    return newArtist;
   }
 
-  findAll() {
-    return `This action returns all artist`;
+  findAll(): Artist[] {
+    return this.artists;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} artist`;
+  findOne(id: string): Artist {
+    const artist = this.artists.find((a) => a.id === id);
+    if (!artist) {
+      throw new NotFoundException(`Artist with ID ${id} not found`);
+    }
+    return artist;
   }
 
-  update(id: number, updateArtistDto: UpdateArtistDto) {
-    return `This action updates a #${id} artist`;
+  update(id: string, updateArtistDto: UpdateArtistDto): Artist {
+    const artist = this.findOne(id);
+    if (updateArtistDto.name) {
+      artist.name = updateArtistDto.name;
+    }
+    if (updateArtistDto.grammy !== undefined) {
+      artist.grammy = updateArtistDto.grammy;
+    }
+    return artist;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} artist`;
+  remove(id: string): void {
+    const artistIndex = this.artists.findIndex((u) => u.id === id);
+    if (artistIndex === -1) {
+      throw new NotFoundException(`Artist with ID ${id} not found`);
+    }
+    this.artists.splice(artistIndex, 1);
   }
 }
