3d75987c6a0514034c13a14155704f7eaf8b08a7
Kate <ekaterina21051988@yandex.by>
2024-03-14 18:39:32 +0200

    feat: The TRACK module has been modified according to Typeorm.

--- src/track/track.service.ts
+++ src/track/track.service.ts
@@ -9,75 +9,66 @@ import { Track } from './entities/track.entity';
 import { v4 as uuidv4 } from 'uuid';
 import { isValidID } from '../helpers/id_validation';
 import { database } from '../database/db';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class TrackService {
-  create(createTrackDto: CreateTrackDto): Track {
-    const { name, artistId, albumId, duration } = createTrackDto;
-    if (!name || !duration) {
-      throw new BadRequestException('Name and Duration are required');
-    }
+  constructor(
+    @InjectRepository(Track)
+    private readonly trackRepository: Repository<Track>,
+  ) {}
 
-    const newTrack: Track = {
-      id: uuidv4(),
-      name,
-      artistId,
-      albumId,
-      duration,
-    };
-    database.tracks.push(newTrack);
-    return newTrack;
+  async findAll(): Promise<Track[]> {
+    return await this.trackRepository.find();
   }
 
-  findAll() {
-    return database.tracks;
-  }
-
-  findOne(id: string): Track {
-    isValidID(id);
-    const track = database.tracks.find((t) => t.id === id);
+  async findOne(id: string): Promise<Track> {
+    const track = await this.trackRepository.findOne({ where: { id } });
     if (!track) {
       throw new NotFoundException(`Track with ID ${id} not found`);
     }
     return track;
   }
 
-  update(id: string, updateTrackDto: UpdateTrackDto): Track {
-    isValidID(id);
-    const track = this.findOne(id);
-    if (!track) {
-      throw new NotFoundException(`Track with ID ${id} not found`);
+  async create(createTrackDto: CreateTrackDto): Promise<Track> {
+    const { name, artistId, albumId, duration } = createTrackDto;
+    if (!name || !duration) {
+      throw new BadRequestException('Name and Duration are required');
     }
-    if (
-      typeof updateTrackDto.name !== 'string' ||
-      typeof updateTrackDto.duration !== 'number'
-    ) {
-      throw new BadRequestException('Invalid dto');
-    }
+
+    const track = this.trackRepository.create({
+      name,
+      artistId,
+      albumId,
+      duration,
+    });
+    return await this.trackRepository.save(track);
+  }
+
+  async update(id: string, updateTrackDto: UpdateTrackDto): Promise<Track> {
+    const track = await this.findOne(id);
+
     if (updateTrackDto.name) {
       track.name = updateTrackDto.name;
     }
     if (updateTrackDto.artistId !== undefined) {
       track.artistId = updateTrackDto.artistId;
     }
-
     if (updateTrackDto.albumId !== undefined) {
       track.albumId = updateTrackDto.albumId;
     }
-
     if (updateTrackDto.duration !== undefined) {
       track.duration = updateTrackDto.duration;
     }
 
-    return track;
+    return await this.trackRepository.save(track);
   }
 
-  remove(id: string) {
-    isValidID(id);
-    const trackIndex = database.tracks.findIndex((u) => u.id === id);
-    if (trackIndex === -1) {
+  async remove(id: string): Promise<void> {
+    const result = await this.trackRepository.delete(id);
+    if (result.affected === 0) {
       throw new NotFoundException(`Track with ID ${id} not found`);
     }
-    database.tracks.splice(trackIndex, 1);
   }
 }
